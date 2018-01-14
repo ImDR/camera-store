e86980ed6e7b815a455b250ee343b2d3e7e96496
Dinesh Rawat <Dineshrawataias@gmail.com>
2018-01-14 16:23:16 +0530

    Add files via upload

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -48,10 +48,15 @@ class UserController extends Controller
 
         $product = Product::find($request->input('product_id'));
 
+        $count = Auth::user()->cart()->wherePivot('product_id', $request->input('product_id'))->get()->count();
+
         if($product){
-            if(Auth::user()->cart()->wherePivot('product_id', $request->input('product_id'))->get()->count()==0){
+            if($count==0 && $request->input('qty')>0){
                 Auth::user()->cart()->attach($product, ['qty'=>$request->input('qty')]);
                 return response()->json(['status' => 'success', 'message'=> 'item successfully added to your cart.']);
+            }elseif($count > 0 && $request->input('qty') == 0){
+                Auth::user()->cart()->detach($product);
+                return response()->json(['status' => 'success', 'message'=> 'item successfully removed from cart.']);
             }else{
                 Auth::user()->cart()->updateExistingPivot($product->getKey(), ['qty'=>$request->input('qty')]);
                 return response()->json(['status' => 'success', 'message'=> 'Your shopping cart was successfully updated.']);
